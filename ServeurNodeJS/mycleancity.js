0d1d36c52ef9df773d1029bbd74092b8f5ebd637
owi0wi <gangan35@hotmail.fr>
2015-05-08 17:23:40 +0200

    added module + modif main

--- ServeurNodeJS/mycleancity.js
+++ ServeurNodeJS/mycleancity.js
@@ -1,14 +1,47 @@
 var express = require('express');
+var session = require('cookie-session'); // Charge le middleware de sessions
+var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var app = express();
 
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Vous êtes à l\'accueil');
-});
+/* On utilise les sessions */
+app.use(session({secret: 'todotopsecret'}))
 
-app.post('/:params', function(req, res){
-	res.setHeader('Content-Type', 'text/plain');
-    res.end(req.params.etagenum['nom']);
-});
 
-app.listen(8080);
+/* S'il n'y a pas de todolist dans la session,
+on en crée une vide sous forme d'array avant la suite */
+.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.txtlist = [];
+    }
+    next();
+})
+
+/* On affiche la txtlist et le formulaire */
+.get('/index', function(req, res) { 
+    res.render('index.ejs', {txtlist: req.session.txtlist});
+})
+
+/* On ajoute un élément à la txtlist */
+.post('/index/ajouter/', urlencodedParser, function(req, res) {
+    if (req.body.newtodo != '') {
+        req.session.txtlist.push(req.body.newtodo);
+    }
+    res.redirect('/index');
+})
+
+/* Supprime un élément de la txtlist */
+.get('/index/supprimer/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.txtlist.splice(req.params.id, 1);
+    }
+    res.redirect('/index');
+})
+
+/* On redirige vers la txtlist si la page demandée n'est pas trouvée */
+.use(function(req, res, next){
+    res.redirect('/index');
+})
+
+.listen(8080);
