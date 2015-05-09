e1648ba9817e3259bf505518edfb81e629fd4c2c
owi0wi <gangan35@hotmail.fr>
2015-05-09 15:03:15 +0200

    supp images inutiles + modif controller listing

--- mycleancity/application/controllers/listingController.php
+++ mycleancity/application/controllers/listingController.php
@@ -17,22 +17,48 @@ class ListingController extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	 public function __construct()
-	 {
-	 	parent::__construct();
-	 	$this->load->model('listingModel');
-	 }
-	 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('listingModel');
+	}
+
 	public function index()
 	{
-		$data['list'] = $this->listingModel->selectAll();
+		$data['list'] = $this->listing();
 		//$data['list'] = 'lolilol';
 
 		$this->load->view('header');
 		$this->load->view('listingView', $data);
 		$this->load->view('footer');
-	
+
 	}
 
+	function listing(){
+		$list = $this->listingModel->selectAll();
+		$listing = array();
+		foreach (get_object_vars($list) as $fkey => $fvalue){
+			if($fkey =='rows'){
+				foreach ($fvalue as $key => $value) {
+					$reponse = $this->listingModel->select($value->id);
+					array_push($listing, $reponse);
+				}
+			}
+		}
+	}
+
+	function update($id){
+		$doc = $this->listingModel->select($id);
+		foreach ($_POST as $key => $value) {
+			if($key != 'id'){
+				$doc->$key = $_POST[$key];
+			}
+		}
+		$this->listingModel->update($doc);
+	}
+	function delete($id){
+		$doc = $this->listingModel->select($id);
+		$this->listingModel->delete($doc);
+	}
 }
 ?>
