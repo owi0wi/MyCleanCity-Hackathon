eafac21e9d41fc0a71c93ca81965ef689341f538
Noux <tavenard.louison@gmail.com>
2015-05-09 22:58:36 +0200

    maps 2

--- mycleancity/application/views/moderationView.php
+++ mycleancity/application/views/moderationView.php
@@ -20,11 +20,10 @@ if (!empty($list)) {
         echo '<div class="col-lg-12">';
         echo '<img class="img-responsive img-border img-left" src="'. $objet->path .'" alt="">';
         echo '<hr class="visible-xs">';
-        echo '<div id="map"><script> var map = L.map('map').setView([51.505, -0.09], 13);
+        echo '<div id="map"><script> var map = L.map(\'map\').setView([51.505, -0.09], 13);
                                                 L.tileLayer(\'http://{s}.tiles.mapbox.com/v3/MapID/{z}/{x}/{y}.png\', {
                                         attribution: \'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>\',
-                                        maxZoom: 18
-                                        }).addTo(map);
+                                        maxZoom: 18}).addTo(map);
 
 
 
