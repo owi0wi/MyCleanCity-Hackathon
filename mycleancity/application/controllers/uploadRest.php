a0b37867fc0f911c43a6ca37377b3cce6dbfffeb
Noux <tavenard.louison@gmail.com>
2015-05-09 11:53:55 +0200

    insert bdd by rest api

--- mycleancity/application/controllers/uploadRest.php
+++ mycleancity/application/controllers/uploadRest.php
@@ -36,33 +36,32 @@ class UploadRest extends REST_Controller
    function upload_post(){
    	$destination = "C:/wamp/www/mycleancity-hackathon/mycleancity/pictures/";
    	$insertDestination = 'c:/wamp/www/mycleancity-hackathon/mycleancity/pictures/clean';
+   	//$destination = "C:/wamp/www/MCC/mycleancity/pictures/";
+   	//$insertDestination = 'c:/wamp/www/MCC/mycleancity/pictures/clean';
    	$extension = ".png";
 	$nbPictures = 0;
-   		//if($_POST){
-			// $ret = false;
-			$img_blob = '';
-			$img_taille = 0;
-			$img_type = '';
-			$img_nom = '';
-			
-			if($dossier = opendir($destination)){
-				while(false !== ($fichier = readdir($dossier))){
-					if($fichier != '.' && $fichier != '..'){
-						$nbPictures +=1;
-					}
-				}	
-			}
-			$destination .= "clean".$nbPictures.$extension;
-			$insertDestination .= $nbPictures.$extension;
+   	if($_POST && $_FILES){	
+		if($dossier = opendir($destination)){
+			while(false !== ($fichier = readdir($dossier))){
+				if($fichier != '.' && $fichier != '..'){
+					$nbPictures +=1;
+				}
+			}	
+		}
+		$destination .= "clean".$nbPictures.$extension;
+		$insertDestination .= $nbPictures.$extension;
 
-			array_push($_POST['data'],array("path" => $insertDestination));
-			$img_taille = $_FILES['picture']['size'];
-			$img_type = $_FILES['picture']['type'];
-			$img_nom = $_FILES['picture']['name'];
-			if(move_uploaded_file ( $_FILES['picture']['tmp_name'] ,$destination)){
-				$nbPictures += 1;
-				$this->uploadModel->insert(json_encode($_POST));
-				$this->response(json_encode(array('reponse' => 'ok')), 200);
-			}
+		$json = json_encode($_POST);
+		print_r(json_decode($json));
+		$_POST["path"] = $insertDestination;
+
+		$img_taille = $_FILES['picture']['size'];
+		$img_type = $_FILES['picture']['type'];
+		$img_nom = $_FILES['picture']['name'];
+		if(move_uploaded_file ( $_FILES['picture']['tmp_name'] ,$destination)){
+			$nbPictures += 1;
+			$this->uploadModel->insert(json_encode($_POST));
+			$this->response(json_encode(array('reponse' => 'ok')), 200);
+		}
    }
 }
