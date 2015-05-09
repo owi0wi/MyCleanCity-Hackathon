2fd3d023f370f1de9a5b0d29752b3727f5f114ac
Noux <tavenard.louison@gmail.com>
2015-05-10 00:29:03 +0200

    vdsvwsfb

--- mycleancity/application/views/moderationView.php
+++ mycleancity/application/views/moderationView.php
@@ -42,7 +42,9 @@ if (!empty($list)) {
 <script>
         var map = L.map('map').setView([51.505, -0.09], 13);
         //http://{s}.tiles.mapbox.com/v3/MapID/{z}/{x}/{y}.png' 
-        L.tileLayer('https://a.tiles.mapbox.com/v4/noux.m502338o/page.html?access_token=pk.eyJ1Ijoibm91eCIsImEiOiJyY0xMaUpVIn0.Wf6E2HX12J5M-XvIGlaA_g#2/-22.4/24.3', {
+        //https://a.tiles.mapbox.com/v4/noux.m502338o/page.html?access_token=pk.eyJ1Ijoibm91eCIsImEiOiJyY0xMaUpVIn0.Wf6E2HX12J5M-XvIGlaA_g#2/-22.4/24.3'
+       
+        L.tileLayer(' http://api.tiles.mapbox.com/v4/noux.m502338o/51.505,-0.09,13/100x100.png?access_token=<pk.eyJ1Ijoibm91eCIsImEiOiJyY0xMaUpVIn0.Wf6E2HX12J5M-XvIGlaA_g>, {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18
         }).addTo(map);
