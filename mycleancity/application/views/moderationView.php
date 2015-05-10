9f05fb081c7a306dc4ef6ec38d378094c0537032
owi0wi <gangan35@hotmail.fr>
2015-05-10 03:12:20 +0200

    a

--- mycleancity/application/views/moderationView.php
+++ mycleancity/application/views/moderationView.php
@@ -73,15 +73,11 @@ L.mapbox.featureLayer({
         // coordinates here are in longitude, latitude order because
         // x, y is the standard for GeoJSON and many formats
         coordinates: [
-          <?php echo $objet->lat; ?>,
-          <?php echo $objet->lon; ?>
+          <?php echo $objet->lon; ?>,
+          <?php echo $objet->lat; ?>
         ]
     },
     properties: {
-        title: 'Peregrine Espresso',
-        description: '1718 14th St NW, Washington, DC',
-        // one can customize markers by adding simplestyle properties
-        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
         'marker-size': 'large',
         'marker-color': '#BE9A6B',
         'marker-symbol': 'bus'
